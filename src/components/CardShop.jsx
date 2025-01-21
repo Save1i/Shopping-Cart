e8c30c92fc6c718dbfb78e5402cf1a2041352cd3
Savelii Kosevich <savva0512@gmail.com>
2025-01-21 12:42:46 +0300

    updating components to display both arrays and single values

--- src/components/CardShop.jsx
+++ src/components/CardShop.jsx
@@ -1,48 +1,26 @@
-import { useEffect, useState } from "react";
-import { Image } from "./Image";
-
-const useCard = (url) => {
-  const [title, setTitle] = useState(null);
-  const [price, setPrice] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Server error");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        // Проверяем тип ответа: массив или объект
-        if (Array.isArray(data)) {
-          const titles = data.map((item) => item["title"]);
-          const prices = data.map((item) => item["price"]);
-          setTitle(titles);
-          setPrice(prices);
-        } else {
-          setTitle([data["title"]]);
-          setPrice([data["price"]]);
-        }
-      })
-      .catch(setError)
-      .finally(() => setLoading(false));
-  }, [url]);
-
-  return { title, price, error, loading };
-};
+import { useCard } from "./useCard";
 
 export const CardShop = ({ url }) => {
-  const { title, price, error, loading } = useCard(url);
+  const { products, error, loading } = useCard(url);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>A network error was encountered</p>;
 
   return (
     <div>
-      <Image url={"https://fakestoreapi.com/products/1"} />
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+const ProductCard = ({ product }) => {
+  const { title, price, image } = product;
+
+  return (
+    <div>
+      <img src={image} alt={title} />
       <h2>{title}</h2>
       <p>{price}</p>
     </div>
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,17 +1,15 @@
-import { useImageURL } from "../hooks/useImageURL";
+// import { useImageURL } from "../hooks/useImageURL";
 import { v4 as uuidv4 } from "uuid";
 
-export const Image = ({ url }) => {
-  const { imageURLs, loading, error } = useImageURL(url, "image");
+export const Image = ({ url, loading, error }) => {
+  // const { imageURLs, loading, error } = useImageURL(url, "image");
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>A network error was encountered</p>;
 
   return (
     <>
-      {imageURLs.map((imageURL, index) => (
-        <img key={uuidv4()} src={imageURL} alt={`Product ${index + 1}`} />
-      ))}
+      <img key={uuidv4()} src={url} alt={`Product`} />
     </>
   );
 };
