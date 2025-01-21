20f695f3655dde68feec86a2cd72fb397d6f5413
Savelii Kosevich <savva0512@gmail.com>
2025-01-21 11:49:55 +0300

    card display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 
+import { CardShop } from "./components/CardShop";
+
 function App() {
-  return;
+  return <CardShop url={"https://fakestoreapi.com/products/1"} />;
 }
 
 export default App;
