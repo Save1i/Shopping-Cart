c437bb2b3d2c1776ef12a06dc4cc43f462472da6
Savelii Kosevich <savva0512@gmail.com>
2025-01-22 18:23:28 +0300

    added provider for data transfer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
+import { router } from "./components/routes";
+import { RouterProvider } from "react-router-dom";
+import { CartProvider } from "./hooks/CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
   </React.StrictMode>
 );
