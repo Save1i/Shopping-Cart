568f23e475a87ec7f70b6b52dd8ccaa9bd8128c0
Savelii Kosevich <savva0512@gmail.com>
2025-01-22 18:24:46 +0300

    added styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,16 @@
 import { BasketIcon } from "./BasketIcon";
+import styles from "../styles/navbar.module.css";
 
 export const Navbar = () => {
   return (
-    <div>
-      <div>
-        <ul>
+    <div className={styles.navbar}>
+      <div className={styles.navbar__inner}>
+        <ul className={styles.navbar__list}>
           <li>shop</li>
           <li>abot us</li>
         </ul>
         <h1>LOGO</h1>
-        <ul>
+        <ul className={styles.navbar__list}>
           <li>search</li>
           <BasketIcon url={"https://fakestoreapi.com/carts"} />
         </ul>
