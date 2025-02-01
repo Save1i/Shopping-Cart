6f452b9e4d4587d40475b90cfa9f5727f82fe779
Savelii Kosevich <savva0512@gmail.com>
2025-02-01 11:12:26 +0300

    change styles

--- src/components/CardShopSkeleton.jsx
+++ src/components/CardShopSkeleton.jsx
@@ -6,12 +6,16 @@ export const CardShopSkeleton = ({ cards }) => {
     .fill(0)
     .map((_, i) => (
       <div className={styles.card} key={i}>
-        <Skeleton sx={{ transform: "none" }} width={273} height={280} />
+        <Skeleton sx={{ transform: "none", borderRadius: "0" }} width={273} height={280} />
         <div className={styles.card__text}>
           <Skeleton sx={{ transform: "none" }} height={20} />
           <Skeleton sx={{ transform: "none", margin: "5px 0 5px 0" }} height={22} />
         </div>
-        <Skeleton sx={{ transform: "none", margin: "0 auto" }} height={36} width={140} />
+        <Skeleton
+          sx={{ transform: "none", margin: "0 auto", borderRadius: "0" }}
+          height={38}
+          width={140}
+        />
       </div>
     ));
 };
