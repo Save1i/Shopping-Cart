19c747ac1a4842800e8efe9a5302075b95e58f9b
Savelii Kosevich <savva0512@gmail.com>
2025-02-01 10:51:38 +0300

    added custon skeleton

--- src/components/CardShop.jsx
+++ src/components/CardShop.jsx
@@ -3,12 +3,17 @@ import { useCard } from "../hooks/useCard";
 import styles from "../styles/cardShop.module.css";
 import { AddToCartBtn } from "./AddToCartBtn";
 import { Skeleton } from "@mui/material";
+import { CardShopSkeleton } from "./CardShopSkeleton";
 
 export const CardShop = ({ url }) => {
-  const { products } = useCard(url);
+  const { products, loading, error } = useCard(url);
+
+  if (error) return <p>A network error was encountered</p>;
 
   return (
     <>
+      {loading && <CardShopSkeleton cards={8} />}
+
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
@@ -20,7 +25,7 @@ const ProductCard = ({ product }) => {
   const { title, price, images, id } = product;
   const [imageError, setImageError] = useState(false);
   const handleImageError = () => {
-    setImageError(true); // Устанавливаем состояние, если изображение не загружается
+    setImageError(true);
   };
 
   return (
@@ -31,7 +36,7 @@ const ProductCard = ({ product }) => {
         <img className={styles.card__img} src={images[0]} alt={title} onError={handleImageError} />
       )}
       <div className={styles.card__text}>
-        <h2 className={styles.card__title}>{title || <Skeleton />}</h2>
+        <h2 className={styles.card__title}>{title}</h2>
         <p className={styles.card__price}>${price}</p>
       </div>
       <AddToCartBtn id={id} />
