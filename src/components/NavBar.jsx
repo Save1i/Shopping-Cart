0a218391ef6dd944b5d1aa563a49decc9d7e6798
Savelii Kosevich <savva0512@gmail.com>
2025-02-02 16:41:08 +0300

    added burger-menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,30 @@
 import { BasketIcon } from "./BasketIcon";
 import styles from "../styles/navbar.module.css";
 import { Link } from "react-router-dom";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const cart = {
+    display: isOpen ? "none" : "block",
+  };
   return (
     <div className={styles.navbar}>
       <div className={styles.navbar__inner}>
+        <button className={styles.burger} onClick={() => setIsOpen(!isOpen)}>
+          <RxHamburgerMenu className={styles.burger__icon} />
+        </button>
+        <ul className={`${styles.burger__list} ${isOpen ? styles.burger__list_open : ""}`}>
+          <li>
+            <Link className={styles.navbar__link} to="/">
+              shop
+            </Link>
+          </li>
+          <li>about us</li>
+          <li>search</li>
+        </ul>
         <ul className={styles.navbar__list}>
           <li>
             <Link className={styles.navbar__link} to="/">
@@ -23,6 +42,9 @@ export const Navbar = () => {
           <li>search</li>
           <BasketIcon url={"https://dummyjson.com/products"} />
         </ul>
+        <div className={styles.basket__icon_burger}>
+          {<BasketIcon style={cart} url={"https://dummyjson.com/products"} />}
+        </div>
       </div>
     </div>
   );
